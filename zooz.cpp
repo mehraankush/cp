7a9269697e6164829952efec32a7eded79030757
ankush <mehraankush36@gmail.com>
2022-09-26 19:45:57 +0530

    zero one equal to one zero

---
+++ zooz.cpp
@@ -0,0 +1,18 @@
+//code chef
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<"1";
+        for(int i=1;i<n-1;i++){
+              cout<<"0";
+       }
+        cout<<"1";
+
+    }
+    return 0;
+}
